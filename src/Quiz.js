5064a37dffccac52e3a69bc1631f87842f9325d8
Robbert van Caem <robbertvancaem@gmail.com>
2019-05-02 16:30:25 +0200

    Add Quiz component

---
+++ src/Quiz.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+class Quiz extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>My quiz title</h1>
+        <p>Amazing description</p>
+      </Fragment>
+    )
+  }
+}
+
+export default Quiz;
